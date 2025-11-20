73ede9954e8684abc626311dd5c3a387956ad2ca
RainRat <rainrat78@yahoo.ca>
2025-11-19 17:51:24 -0800

    Fix width clipping in pseudo-graphical console

--- ui.py
+++ ui.py
@@ -306,8 +306,10 @@ class PseudoGraphicalConsole(ConsoleInterface):
             log_lines = list(self._log_lines)[-remaining_rows:]
             lines_to_draw.extend(log_lines)
 
+        display_width = max(0, width)
         for row, line in enumerate(lines_to_draw[:height]):
-            self._screen.addnstr(row, 0, line, width - 1)
+            clipped_line = line[:display_width]
+            self._screen.addnstr(row, 0, clipped_line, display_width)
 
         self._screen.refresh()
 
